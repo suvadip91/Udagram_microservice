cdb240afbdddfc1422da2668489f3a2bd0111e36
Suvadip <suvadip91@yahoo.com>
2020-12-07 10:45:20 +0530

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=suvadip
+export POSTGRES_PASSWORD=Anandapur39
+export POSTGRES_HOST=suvakubdev.cugh3lqbo6vn.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=suva-kub-dev
+export AWS_REGION=ap-south-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
